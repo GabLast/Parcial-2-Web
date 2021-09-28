70f97b557d3b075dab4de6b253c1bde040d301d5
Gabriel Marte <50205184+GabLast@users.noreply.github.com>
2021-09-28 10:11:03 -0400

    Delete build directory

--- build/resources/main/public/js/Custom.js
+++
@@ -1,5 +0,0 @@
-$(document).ready(function(){
-    $('#procesar').on("click", function() {
-        alert("Su carrito de compras no tiene productos.\nAgregue productos antes de procesar su compra.");
-    });
-});
